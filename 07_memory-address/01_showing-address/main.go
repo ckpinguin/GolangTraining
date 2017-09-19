a69e2d3668a4f159f13c9a55add675be4ce63d43
Christof Kälin <christof.kaelin@gmail.com>
2017-09-19 17:28:00 +0200

    more exps

--- 07_memory-address/01_showing-address/main.go
+++ 07_memory-address/01_showing-address/main.go
@@ -3,10 +3,14 @@ package main
 import "fmt"
 
 func main() {
-
+	const b int = 12
 	a := 43
 
 	fmt.Println("a - ", a)
 	fmt.Println("a's memory address - ", &a)
 	fmt.Printf("%d \n", &a)
+
+	fmt.Println("b - ", b)
+	// fmt.Println("b's memory address - ", &b)
+	// fmt.Printf("%d \n", &b)
 }
