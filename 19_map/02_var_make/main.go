82ab463601a7f8446a5e0ac7a745bb720c0677d3
Christof Kälin <christof.kaelin@gmail.com>
2017-09-21 16:18:50 +0200

    experiments

--- 19_map/02_var_make/main.go
+++ 19_map/02_var_make/main.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 
 	var myGreeting = make(map[string]string)
+	fmt.Println(myGreeting)
+	fmt.Println(myGreeting == nil)
 	myGreeting["Tim"] = "Good morning."
 	myGreeting["Jenny"] = "Bonjour."
 
--- 19_map/04_shorthand_composite-literal/main.go
+++ 19_map/04_shorthand_composite-literal/main.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 
 	myGreeting := map[string]string{}
+	fmt.Println(myGreeting)
+	fmt.Println(myGreeting == nil)
 	myGreeting["Tim"] = "Good morning."
 	myGreeting["Jenny"] = "Bonjour."
 
