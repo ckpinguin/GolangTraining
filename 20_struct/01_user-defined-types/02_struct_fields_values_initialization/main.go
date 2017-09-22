04eee18cb5ad2fbe41ae14a2556dd270521c16b9
Chrigi <ckpinguin@circleci.com>
2017-09-22 14:37:47 +0200

    exps

--- 20_struct/01_user-defined-types/02_struct_fields_values_initialization/main.go
+++ 20_struct/01_user-defined-types/02_struct_fields_values_initialization/main.go
@@ -8,14 +8,9 @@ type person struct {
 	age   int
 }
 
-func (p person) fullName() string {
-	return p.first + p.last
-}
-
 func main() {
 	p1 := person{"James", "Bond", 20}
 	p2 := person{"Miss", "Moneypenny", 18}
-	fmt.Println((person).fullName(p1)) // de-sugared
-	fmt.Println(p1.fullName())
-	fmt.Println(p2.fullName())
+	fmt.Println(p1.first, p1.last, p1.age)
+	fmt.Println(p2.first, p2.last, p2.age)
 }
