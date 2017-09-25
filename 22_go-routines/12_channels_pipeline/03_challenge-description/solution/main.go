9f3c8634a56136d291a848dd2c7a70e60655bcc7
Christof Kälin <christof.kaelin@gmail.com>
2017-09-25 13:08:46 +0200

    exercices & comments

---
+++ 22_go-routines/12_channels_pipeline/03_challenge-description/solution/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	for n := range factorial(gen()) {
+		fmt.Println(n)
+	}
+}
+
+func gen() <-chan int {
+	out := make(chan int)
+	go func() {
+		for i := 0; i < 100; i++ {
+			n := rand.Intn(10)
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func factorial(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- fact(n)
+		}
+		close(out)
+	}()
+	return out
+}
+
+func fact(n int) int {
+	total := 1
+	for i := n; i > 0; i-- {
+		total *= i
+	}
+	return total
+}
+
+/*
+CHALLENGE #1:
+-- Change the code above to execute 100 factorial computations concurrently and in parallel.
+-- Use the "pipeline" pattern to accomplish this
+
+POST TO DISCUSSION:
+-- What realizations did you have about working with concurrent code when building your solution?
+-- eg, what insights did you have which helped you understand working with concurrency?
+-- Post your answer, and read other answers, here: https://goo.gl/uJa99G
+*/
--- 22_go-routines/12_channels_pipeline/04_challenge-solution/02_another-solution/main.go
+++ 22_go-routines/12_channels_pipeline/04_challenge-solution/02_another-solution/main.go
@@ -21,25 +21,25 @@ func factorial(wmain *sync.WaitGroup) {
 	var w sync.WaitGroup
 	rand.Seed(42)
 
-	w.Add(numFactorials + 1)
+	w.Add(numFactorials + 1) // why adding this again to another WaitGroup?
 
 	for j := 1; j <= numFactorials; j++ {
 
 		go func() {
 			f := rand.Intn(rdLimit)
-			w.Wait()
+			w.Wait() // wait for all being initialized
 			total := 1
 			for i := f; i > 0; i-- {
 
 				total *= i
 			}
-			fmt.Println(f, total)
-			(*wmain).Done()
+			fmt.Println(f, total) // no channels used here, so we print now
+			(*wmain).Done()       // one thread done
 			//out <- total
 
 		}()
 		w.Done()
 	}
-	fmt.Println("All done with initialization")
-	w.Done()
+	fmt.Println("All done with initialization, starting with calculations...")
+	w.Done() // this is the +1 wait thread
 }
